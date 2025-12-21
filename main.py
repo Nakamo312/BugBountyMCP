dde3c7fdb458b2a90c5c0484f8c6a1ef08528e83
Nakamo312 <shehmatov.vitek@gmail.com>
2025-12-21 15:51:45 +0300

    added global exception handllers

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """Application entry point"""
 import uvicorn
-from api.presentation.rest.app import create_app
-from api.config import settings
+from src.api.presentation.rest.app import create_app
+from src.api.config import settings
 
 if __name__ == "__main__":
     app = create_app()
