847b8725cc9ec68ade3d483e001317248dda5acd
Nakamo312 <shehmatov.vitek@gmail.com>
2025-12-29 12:04:38 +0300

    optimization threads workers

--- src/api/infrastructure/repositories/adapters/header.py
+++ src/api/infrastructure/repositories/adapters/header.py
@@ -1,5 +1,7 @@
 """Header repository"""
 
+from uuid import UUID
+
 from api.domain.models import HeaderModel
 from api.infrastructure.repositories.interfaces.base import AbstractRepository
 from api.infrastructure.repositories.adapters.base import SQLAlchemyAbstractRepository
@@ -8,6 +10,24 @@ from api.infrastructure.repositories.adapters.base import SQLAlchemyAbstractRepo
 
 class SQLAlchemyHeaderRepository(SQLAlchemyAbstractRepository, AbstractRepository[HeaderModel]):
     """Repository for Header entities"""
-    
+
     model = HeaderModel
     unique_fields = [("endpoint_id", "name")]
+
+    async def ensure(
+        self,
+        endpoint_id: UUID,
+        name: str,
+        value: str,
+    ) -> HeaderModel:
+        entity = HeaderModel(
+            endpoint_id=endpoint_id,
+            name=name,
+            value=value
+        )
+
+        return await self.upsert(
+            entity,
+            conflict_fields=["endpoint_id", "name"],
+            update_fields=["value"]
+        )
