e4e16f8e9b352351ff85c72a55e861300c2c4c88
Nakamo312 <shehmatov.vitek@gmail.com>
2026-01-04 21:38:42 +0300

    implemented dnsx service

--- src/api/application/dto/__init__.py
+++ src/api/application/dto/__init__.py
@@ -1,9 +1,18 @@
 
-from .scan_dto import HTTPXScanInputDTO, HTTPXScanOutputDTO, SubfinderScanInputDTO, SubfinderScanOutputDTO
+from .scan_dto import (
+    HTTPXScanInputDTO,
+    HTTPXScanOutputDTO,
+    SubfinderScanInputDTO,
+    SubfinderScanOutputDTO,
+    DNSxScanInputDTO,
+    DNSxScanOutputDTO,
+)
 
 __all__ = [
     'HTTPXScanInputDTO',
     'HTTPXScanOutputDTO',
     'SubfinderScanInputDTO',
     'SubfinderScanOutputDTO',
+    'DNSxScanInputDTO',
+    'DNSxScanOutputDTO',
 ]
