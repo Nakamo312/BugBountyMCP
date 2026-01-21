a988e9f036b38aea03a6b30ea689d46f1bacaafc
Nakamo312 <shehmatov.vitek@gmail.com>
2026-01-21 06:42:39 +0300

    refactoring dashboard scan page

--- BugBountyDashBoard/src/pages/Scans/ScanPage.jsx
+++ BugBountyDashBoard/src/pages/Scans/ScanPage.jsx
@@ -1,12 +1,10 @@
-import { SCANS } from '@/components/scans'
+// ScansPage.jsx
+import { useProgram } from '@/context/ProgramContext'
 import { useScanRunner } from '@/components/scans/hooks/useScanRunner'
-import ScanGrid from '@/components/scans/ui/ScanGrid'
-import ScanCard from '@/components/scans/ui/ScanCard'
-import ScanFormFactory from '@/components/scans/ui/ScanFormFactory'
 
 export default function ScansPage() {
-  const scanRunner = useScanRunner()
-
+  const { selectedProgram } = useProgram()   
+  const scanRunner = useScanRunner(selectedProgram)
   return (
     <ScanGrid>
       {SCANS.map(scan => (
